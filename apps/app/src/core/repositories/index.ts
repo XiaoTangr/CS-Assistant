1c7dbcb2c9cad0783c48dc0aa0d57cb69115796d
XiaoTang <3083649887@qq.com>
2025-07-27 15:19:58 +0800

    add exports ts files

---
+++ apps/app/src/core/repositories/index.ts
@@ -0,0 +1,5 @@
+export { default as BackupAndRecoveryRepository } from "./BAR.Repository"
+
+export { default as SettingsRepository } from "./Settings.Repository";
+
+export { default as MapRepository } from "./Map.Repository";
