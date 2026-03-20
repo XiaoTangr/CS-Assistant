8a38d56ea1e252069154ee85863f099113c977a7
XiaoTang <3083649887@qq.com>
2026-03-20 10:05:18 +0800

    添加社区内容后端初始化

--- app/src-tauri/build.rs
+++
@@ -1,3 +0,0 @@
-pub fn main() {
-    tauri_build::build()
-}
--- app/src/router/Router.ts
+++
@@ -1,7 +0,0 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { routesConfig } from '../core/config/routes-config'
-
-export const MainRouter = createRouter({
-    history: createWebHistory(),
-    routes: [...routesConfig],
-})
